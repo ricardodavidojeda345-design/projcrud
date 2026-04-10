ba021792af40d4778890dea25be1ab7b45e0f988
ricardodavidojeda345-design <ricardodavidojeda345@gmail.com>
2026-04-10 12:13:30 -0300

    Update index_model.php

--- index_model.php
+++ index_model.php
@@ -1,24 +1,11 @@
 <?php
 $conn = new \PDO("sqlite:banco.db");
 
+$sql_ferramentas = "
+SELECT id, nome, categoria, descricao, link, gratuita
+FROM ferramentas
+ORDER BY id DESC
+";
 
-$result_set = $conn->query("
-SELECT nome, categoria, descricao, link, gratuita
-FROM ferramentas;
-");
-$result_set_ferramentas = $conn->query("
-SELECT nome, categoria, descricao, link, gratuita
-FROM ferramentas;
-");
-
-
-$uma_linha = $result_set->fetch(\PDO::FETCH_ASSOC);
-
-
-$nome = $uma_linha["nome"]; 
-$categoria = $uma_linha["categoria"]; 
-$descricao = $uma_linha["descricao"]; 
-$link = $uma_linha["link"]; 
-$gratuito = $uma_linha["gratuita"]; 
-
+$result_set_ferramentas = $conn->query($sql_ferramentas);
 ?>
